f2fd426b6f502f45bc00801c144840195839cd2c
salik <salik.e@lookupitsolutions.in>
2021-06-24 17:41:15 +0530

    Catalog and BasketAPI

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,14 +1,7 @@
-using Catalog.API;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Catalog.ApI
+namespace Basket.API
 {
     public class Program
     {
@@ -19,6 +12,7 @@ namespace Catalog.ApI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                //.UseSerilog(SeriLogger.Configure)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
